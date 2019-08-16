6869f6c1c2bebdc76072395de09aac40114f4272
Woo Jia Hao <woojiahao1234@gmail.com>
2019-08-16 20:49:47 +0800

    Fixed aur download bugs

--- manjaro-setup.sh
+++ manjaro-setup.sh
@@ -2,18 +2,17 @@
 # This is the installation script for all the software used for Manjaro
 # This function is to retrieve an AUR package and install it 
 aur_download () {
-  cd ~/Downloads/
   AUR="https://aur.archlinux.org/$1.git"
-  git clone AUR && cd $1 && makepkg -sic && cd .. && rm -rf $1 
+  git clone $AUR && cd $1 && makepkg -sic && cd .. && sudo rm -rf $1 
 }
 
 # Update packages first to prevent errors
-pacman -Syuu
+sudo pacman -Syuu
 # Install all applications compatible with pacman
 # TODO: Create script to setup postgresql
-pacman -S vim jre11-openjdk jdk11-openjdk openjdk11-doc telegram-desktop libreoffice-still gimp gcc make docker
-pacman -S linux$(uname -r|sed 's/\W//g'|cut -c1-3)-headers
-pacman -S snapd && systemctl enable --now snapd.socket && ln -s /var/lib/snapd/snap /snap
+sudo pacman -S vim jre11-openjdk jdk11-openjdk openjdk11-doc telegram-desktop libreoffice-still gimp gcc make docker
+sudo pacman -S linux$(uname -r|sed 's/\W//g'|cut -c1-3)-headers
+sudo pacman -S snapd && systemctl enable --now snapd.socket && ln -s /var/lib/snapd/snap /snap
 
 # Install all applicatons found in the AUR
 aur_download "google-chrome"
@@ -28,8 +27,8 @@ aur_download "skypeforlinux-stable-bin"
 # Install all applications found in snap 
 pacman -Qi snapd
 if [ $? -eq 0 ]; then
-  snap install slack --classic
+  sudo snap install slack --classic
 fi 
 
 # Reboot OS
-reboot
+sudo reboot
