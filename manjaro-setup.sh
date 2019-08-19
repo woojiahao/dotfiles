ec61963cc7acd0eb974ec9331cf89433452b3df9
Woo Jia Hao <woojiahao1234@gmail.com>
2019-08-19 23:03:31 +0800

    Added dual Java installation

--- manjaro-setup.sh
+++ manjaro-setup.sh
@@ -3,25 +3,40 @@
 # This function is to retrieve an AUR package and install it 
 aur_download () {
   AUR="https://aur.archlinux.org/$1.git"
-  git clone $AUR && cd $1 && makepkg -sic --noconfirm && cd .. && sudo rm -rf $1 
+  git clone $AUR 
+  cd $1 
+  makepkg -sic --noconfirm 
+  cd .. 
+  sudo rm -rf $1 
 }
 
 # Update packages first to prevent errors
 sudo pacman -Syuu
+
 # Install all applications compatible with pacman
-# Set up should not display prompts
-# TODO: Create script to setup postgresql
-sudo pacman -S --noconfirm base-devel vim jre11-openjdk jdk11-openjdk openjdk11-doc telegram-desktop libreoffice-still gimp gcc make noto-fonts-emoji discord xclip neofetch
-sudo pacman -S --noconfirm linux$(uname -r|sed 's/\W//g'|cut -c1-3)-headers
+# Install essential packages
+echo "Installing essential packages"
+sudo pacman -S --noconfirm base-devel gcc make linux$(uname -r|sed 's/\W//g'|cut -c1-3)-headers
+
+# Install both Java 8 and 11 - use Java 11 as default while Java 8 is used for development purposes
+echo "Installing Java 8 & 11 and set Java 11 as default"
+sudo pacman -S --noconfirm jre8-openjdk jdk8-openjdk openjdk8-doc jre11-openjdk jdk11-openjdk openjdk11-doc
+archlinux-java set java-11-openjdk
+
+# Install all the basic applications
+echo "Installing basic application"
+sudo pacman -S --noconfirm vim telegram-desktop libreoffice-still gimp noto-fonts-emoji discord xclip neofetch
 sudo pacman -S --noconfirm snapd && systemctl enable --now snapd.socket && ln -s /var/lib/snapd/snap /snap
 
 # Set up Docker (Sets the docker service to run on boot too)
+echo "Installing Docker"
 sudo pacman -S docker docker-machine docker-compose
 systemctl start docker
 systemctl enable docker
 sudo gpasswd -a $USERNAME docker
 
 # Set up Ruby
+echo "Installing Ruby"
 sudo pacman -S ruby ruby-irb
 sudo export PATH="$PATH:$(ruby -e 'puts Gem.user_dir')/bin"
 
